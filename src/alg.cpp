f9f9c106521a138168a47a1889bbc42799ef0bee
DmitriiObushko <105298559+DmitriiObushko@users.noreply.github.com>
2023-03-26 23:07:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,43 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
     int search = 0;
-
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                search += 1;
+                search = search + 1;
             }
         }
     }
-
     return search;
 }
 int countPairs2(int* arr, int len, int value) {
     int search = 0;
-
     for (int i = 0; i < len - 1; i++) {
         for (int j = len - 1; i < j; j--) {
             if (arr[i] + arr[j] == value) {
-                search += 1;
+                search = search + 1;
             }
         }
     }
-
     return search;
 }
 int countPairs3(int* arr, int len, int value) {
     int search = 0;
-
     for (int i = 0; i < len - 1; i++) {
         int first = i, second = len;
-
         while (first < second - 1) {
             int ctr = (first + second) / 2;
-
             if (arr[i] + arr[ctr] == value) {
-                search += 1;
-
+                search = search + 1;
                 int nCtr = ctr + 1;
-
                 while (arr[i] + arr[nCtr] == value && nCtr < second) {
-
-                    search += 1;
-                    nCtr += 1;
-
+                    search = search + 1;
+                    nCtr = nCtr + 1;
                 }
                 nCtr = ctr - 1;
                 while (arr[i] + arr[nCtr] == value && nCtr > first) {
-                    search += 1;
-                    nCtr -= 1;
+                    search = search + 1;
+                    nCtr = nCtr - 1;
                 }
                 break;
             }
@@ -60,6 +49,5 @@ int countPairs3(int* arr, int len, int value) {
             }
         }
     }
-
     return search;
 }
